1e74d8792587af04e2ff56ad8cf29b0e7fb09309
samuel munyeke <samuelmunyeke.2@gmail.com>
2025-06-08 20:47:17 +0300

    fixed TypeScript errors

--- src/hooks/useSkips.ts
+++ src/hooks/useSkips.ts
@@ -1,11 +1,12 @@
 import { useQuery } from '@tanstack/react-query';
 import { skipService } from '../services/skipService';
+import { SkipOption } from '../types/skip';
 
 export const useSkips = () => {
-  return useQuery({
+  return useQuery<SkipOption[]>({
     queryKey: ['skips'],
     queryFn: skipService.getSkips,
     staleTime: 5 * 60 * 1000, // Consider data fresh for 5 minutes
-    cacheTime: 30 * 60 * 1000, // Keep data in cache for 30 minutes
+    gcTime: 30 * 60 * 1000, // Keep data in cache for 30 minutes
   });
 }; 
